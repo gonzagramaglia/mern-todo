4af536e9e3434f683556f12e5a72f9ddc0811bdf
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-12 13:28:19 -0300

    adding and deleting todos

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const app = express();
 
 app.use(express.json()); // allow us to accept JSON data in the req.body
 
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Task.find({});
+    res.status(200).json({ success: true, data: todos });
+  } catch (err) {
+    console.log("Error in fetching todos:", err.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.post("/api/todos", async (req, res) => {
   console.log(req);
   const task = req.body; // user will send this data
@@ -30,6 +40,19 @@ app.post("/api/todos", async (req, res) => {
     console.error(`Error: ${err.message}`);
     res.status(500).json({ success: false, message: `Server Error` });
     process.exit(1); // 1 = failure, 0 = success
+  }
+});
+
+app.delete("/api/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(`id: ${id}`);
+
+  try {
+    await Task.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Todo deleted" });
+  } catch (err) {
+    console.log("Error in deleting todos:", err.message);
+    res.status(404).json({ success: false, message: "Todo not found" });
   }
 });
 
