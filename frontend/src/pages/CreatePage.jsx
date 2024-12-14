28a9dce74a259244762d4604a07a6f43872c44dd
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-14 19:24:34 -0300

    CreatePage: adding a todo

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,95 @@
+import {
+  Container,
+  VStack,
+  Heading,
+  useColorModeValue,
+  Box,
+  Input,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useTaskTodos } from "../todos/task";
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    image: "",
+  });
+
+  const { createTask } = useTaskTodos();
+  const toast = useToast();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createTask(newTask);
+    console.log("Success:", success);
+    console.log("Message:", message);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Container maxW="container.sm">
+        <VStack spacing={8}>
+          <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+            Create New Task
+          </Heading>
+          <Box
+            w={"full"}
+            bg={useColorModeValue("white", "gray.800")}
+            p={6}
+            rounded={"lg"}
+            shadow={"md"}
+          >
+            <VStack spacing={4}>
+              <Input
+                placeholder="Title"
+                name="title"
+                value={newTask.title}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, title: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Description"
+                name="description"
+                value={newTask.description}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, description: e.target.value })
+                }
+              />
+              <Input
+                placeholder="Image URL"
+                name="image"
+                value={newTask.image}
+                onChange={(e) =>
+                  setNewTask({ ...newTask, image: e.target.value })
+                }
+              />
+              <Button colorScheme="blue" onClick={handleAddProduct} w="full">
+                Add Task
+              </Button>
+            </VStack>
+          </Box>
+        </VStack>
+      </Container>
+    </>
+  );
 };
 
 export default CreatePage;
---
+++ frontend/src/todos/task.js
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+export const useTaskTodos = create((set) => ({
+  tasks: [],
+  setTasks: (tasks) => set({ tasks }),
+  createTask: async (newTask) => {
+    if (!newTask.title) {
+      return { success: false, message: "Please add a title" };
+    }
+
+    try {
+      const res = await fetch("/api/todos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to create task");
+      }
+
+      const data = await res.json();
+      set((state) => ({ tasks: [...state.tasks, data.data] }));
+      return { success: true, message: "Task created" };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  },
+}));
