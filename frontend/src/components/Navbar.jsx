4ee71040026ee49f9ac64bf5f3abe934857a68e7
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-13 21:04:21 -0300

    Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,54 @@
+import {
+  Container,
+  Flex,
+  Text,
+  HStack,
+  Button,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Container maxW="1140px" px={4}>
+        <Flex
+          h={28}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexDir={{
+            base: "column",
+            sm: "row",
+          }}
+        >
+          <Text
+            fontSize={{ base: "22", sm: "28" }}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign={""}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+          >
+            <Link to={"/"}>MERN Todo</Link>
+          </Text>
+          <HStack spacing={2} alignItems={""}>
+            <Link to={"/create"}>
+              <Button>
+                <PlusSquareIcon fontSize={20} />
+              </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+            </Button>
+          </HStack>
+        </Flex>
+      </Container>
+    </>
+  );
 };
 
 export default Navbar;
