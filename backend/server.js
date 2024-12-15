6ba6482ce640a4e43683caa4017f3de6b669f372
Gonzalo Gramaglia <gonzagramaglia@icloud.com>
2024-12-15 14:46:48 -0300

    ready to build & start

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import todosRoutes from "./routes/todos.js";
 
@@ -8,9 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8020;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allow us to accept JSON data in the req.body
 
 app.use("/api/todos", todosRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
